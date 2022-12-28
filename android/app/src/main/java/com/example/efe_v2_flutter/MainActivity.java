884de796487405d1a49b8c1d7eda71c2c6b02567
Szántó Benjámin <61885011+vellt@users.noreply.github.com>
2022-12-28 14:35:22 +0100

    first step

---
+++ android/app/src/main/java/com/example/efe_v2_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.efe_v2_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/audio.dart
@@ -0,0 +1,11 @@
+class Audio {
+  late String title;
+  late String voiceUrl;
+  late bool isFavorite;
+
+  Audio({
+    required this.title,
+    required this.voiceUrl,
+    required this.isFavorite,
+  });
+}
